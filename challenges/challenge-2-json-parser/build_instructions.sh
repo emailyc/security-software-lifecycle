9d82534249938e7ce47e528e3b015ddd62831762
Yiu Chung WONG <29031682+emailyc@users.noreply.github.com>
2023-11-21 17:39:24 +0100

    Update build_instructions.sh

--- challenges/challenge-2-json-parser/build_instructions.sh
+++ challenges/challenge-2-json-parser/build_instructions.sh
@@ -3,10 +3,12 @@ export AFL_USE_ASAN=1
 CC=afl-cc CXX=afl-c++ cmake ..
 make clean; make -j 8
 
-export AFL_PRELOAD='/mnt/YAJL/build/yajl-2.1.1/lib' \
-afl-cc -fsanitize=address -o json_reader json_reader.c \
-    -lyajl -I'/mnt/YAJL/build/yajl-2.1.1/include' \
-    -L'/mnt/YAJL/build/yajl-2.1.1/lib' \
-    -I/AFLplusplus/include
+export LD_LIBRARY_PATH=/AFLplusplus/security-software-lifecycle/challenges/challenge-2-json-parser/YAJL/build/yajl-2.1.1/lib/:$LD_LIBRARY_PATH
+afl-cc -fsanitize=address -o json_reader json_reader.c  -lyajl \
+-I /AFLplusplus/include
+-I'/AFLplusplus/security-software-lifecycle/challenges/challenge-2-json-parser/YAJL/build/yajl-2.1.1/include' \
+-L'/AFLplusplus/security-software-lifecycle/challenges/challenge-2-json-parser/YAJL/build/yajl-2.1.1/lib' \
 
 afl-fuzz -i ./seeds -o ./findings -x json.dict -- ./json_reader 
+
+
